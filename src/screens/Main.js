7856624b2782c64c2a539259c6322603f22685e7
husni.codemi <husni.ramdani@codemi.co.id>
2022-02-13 18:35:08 +0700

    header

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Svg from '@components/Svg';
+import {
+  Button,
+} from 'react-native';
+// Screen
 import MotoGP from './MotoGP';
 import Moto2 from './Moto2';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +18,28 @@ export default function MainScreen() {
         headerTransparent: true,
       }}
     >
-      <Tab.Screen name="MotoGP" component={MotoGP} />
+      <Tab.Screen
+        name="MotoGP"
+        component={MotoGP}
+        options={{
+          headerTitleAlign: 'center',
+          headerTitle: () => <Svg icon="gppal_M" size={70} />,
+          headerLeft: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="red"
+            />
+          ),
+          headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="blue"
+            />
+          ),
+        }}
+      />
       <Tab.Screen name="Moto2" component={Moto2} />
     </Tab.Navigator>
   );
